3d09bfc65ece29dc671900532b489a2abc521cdd
Adriano Teixeira Benevenuto Sampaio <adrianoteixeira1223@gmail.com>
2025-09-30 17:22:16 -0300

    feat(ListaUsuario): Adicioano contador de usuarios para menor de idade e maior de idade e total

--- src/components/ListaUsuarios.jsx
+++ src/components/ListaUsuarios.jsx
@@ -1,12 +1,20 @@
 export default function ListaUsuarios({usuarios, excluirUsuario, setUsuarioEditando}) {
-
+    const total = usuarios.length;
+    const maiores = usuarios.filter(u => u.idade >= 18).length;
+    const menores = total - maiores;
    
     return(
         <div className="lista-usuarios">
             <h2>Lista de Usuários</h2>
+            <div className="estatisticas">
+                    <p>Total: {total}</p>
+                    <p>Maiores de 18: {maiores}</p>
+                    <p>Menores de 18: {menores}</p>
+            </div>
             {usuarios.length === 0 ? (
                 <p>Nenhum usuário cadastrado.</p>
             ) : (
+             
                 <ul>
                 {usuarios.map((usuario, index) => (
                     <li key={index}>
