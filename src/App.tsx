3e6666a9b6d9a0e1046550ae6a953cd1b4c7b8a2
Adriano Teixeira Benevenuto Sampaio <adrianoteixeira1223@gmail.com>
2025-09-30 17:53:41 -0300

    feat(App): Adiciona persistência da lista de usuario para localStorage

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,37 @@
-import { useState } from 'react'
+import { useState, useEffect, use } from 'react'
 import './App.css'
 import FormularioUsuario from './components/FormularioUsuario'
 import ListaUsuarios from './components/ListaUsuarios'
 
+type Usuario = {
+  nome: string;
+  email: string;
+  idade: number;
+  cidade: string;
+}
+
 function App() {
-  const [usuarios, setUsuarios] = useState([])
+  const [usuarios, setUsuarios] = useState(() =>{
+    const usuariosSalvos = localStorage.getItem('usuarios')
+    return usuariosSalvos ? JSON.parse(usuariosSalvos) : []
+  })
   const [usuarioEditando, setUsuarioEditando] = useState(null)
   const [filtro, setFiltro] = useState('');
 
-  function adicionarUsuario(usuario) {
+  function adicionarUsuario(usuario: Usuario) {
     setUsuarios([...usuarios, usuario])
   }
-  function excluirUsuario(usuario) {
-    setUsuarios(usuarios.filter(u => u !== usuario))
+  function excluirUsuario(usuario: Usuario) {
+    setUsuarios(usuarios.filter((u: Usuario) => u !== usuario))
   }
-  function editarUsuario(usuarioAntigo, usuarioNovo) {
-    setUsuarios(usuarios.map(u => u === usuarioAntigo ? usuarioNovo : u))
+  function editarUsuario(usuarioAntigo :Usuario, usuarioNovo :Usuario) {
+    setUsuarios(usuarios.map((u: Usuario) => u === usuarioAntigo ? usuarioNovo : u))
     setUsuarioEditando(null)
   }
-  
+  useEffect(() => {
+    localStorage.setItem('usuarios', JSON.stringify(usuarios))
+  }, [usuarios])
+
   return (
     
     <>
@@ -30,7 +43,7 @@ function App() {
       setFiltro={setFiltro}
       />
      <ListaUsuarios 
-    usuarios={usuarios.filter(u => 
+    usuarios={usuarios.filter((u: Usuario) => 
         u.nome.toLowerCase().includes(filtro.toLowerCase()) ||
         u.email.toLowerCase().includes(filtro.toLowerCase()) ||
         u.cidade.toLowerCase().includes(filtro.toLowerCase()) ||
